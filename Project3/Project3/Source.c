130fa0e5a88d3723afe1dd33230914128e8da3ac
adam <adamthewilliams@gmail.com>
2018-12-02 17:33:20 -0600

    first

--- Project3/Project3/Source.c
+++ Project3/Project3/Source.c
@@ -5,24 +5,6 @@
    
 
 
-const char *argp_program_version =
-"argp-ex3 1.0";
-const char *argp_program_bug_address =
-"<bug-gnu-utils@gnu.org>";
-
-
-static char doc[] =
-"Argp example #3 -- a program with options and arguments using argp";
-
-static char args_doc[] = "port ip file transport";
-
-
-struct arguments {
-  char * args[4];
-  int port, ip, file, transport;
-  char *output_file;
-};
-
 static struct argp_option options[] = {
   { "port",      'p', 0, 0, "List of ports to scan" },
   { "ip",        'i', 0, 0, "IP address to scan" },
@@ -51,22 +33,5 @@ static struct argp argp = { options, parse_opt, args_doc, doc };
 
 int main(int argc, char** argv) {
     
-  int i;
-  struct arguments arguments;
-  
-  arguments.file = 0;
-  arguments.ip = 0;
-  arguments.port = 0;
-  arguments.transport = 0;
-  arguments.output_file = "--";
 
-
-  argp_parse(&argp, argc, argv, 0, 0, &arguments);    
-
-  for ( i = 0; i < 4; ++i ) {
-    printf("%s", arguments.args[i]);
-  }
-    
-  char buff[10];
-  scanf("%s", buff);
 }
